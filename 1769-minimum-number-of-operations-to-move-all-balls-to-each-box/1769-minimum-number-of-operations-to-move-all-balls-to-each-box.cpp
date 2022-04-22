e9fae0b84690f69c4793c7a8a80224d294a232b1
bhannu galhotra <42721076+Bhannugalhotra@users.noreply.github.com>
2022-04-22 11:28:15 +0530

    Time: 7 ms (86.40%), Space: 9 MB (77.37%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -6,19 +6,49 @@ public:
         
         vector<int> ans(n);
         
-        for(int i = 0; i < n; i++){
+        // O(n*n) solution
+//         for(int i = 0; i < n; i++){
             
-            int count = 0;
+//             int count = 0;
             
-            for(int j = 0; j < i; j++){
-                count += (boxes[j] - '0') * (i - j);
+//             for(int j = 0; j < i; j++){
+//                 count += (boxes[j] - '0') * (i - j);
+//             }
+            
+//             for(int j = i+1; j < n; j++){
+//                 count += (boxes[j] - '0') * (j - i);
+//             }
+            
+//             ans[i] = count;
+//         }
+        
+        //put balls first from left to right
+        
+        int sum = 0;
+        int balls = 0;
+        
+        for(int i = 0 ; i < n; i++){
+            
+            sum += balls;
+            ans[i] += sum;
+            
+            if(boxes[i] == '1'){
+                balls++;
             }
+        }
+        
+        //now put balls from right from left
+        
+        sum = 0;
+        balls = 0;
+        for(int i = n-1; i >= 0; i--){
             
-            for(int j = i+1; j < n; j++){
-                count += (boxes[j] - '0') * (j - i);
+            sum += balls;
+            ans[i] += sum;
+            
+            if(boxes[i] == '1'){
+                balls++;
             }
-            
-            ans[i] = count;
         }
         
         return ans;

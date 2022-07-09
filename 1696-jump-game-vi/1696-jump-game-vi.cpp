7f33c808a93a95ac2cf3a550936607aadb021f26
bhannu galhotra <42721076+Bhannugalhotra@users.noreply.github.com>
2022-07-09 11:02:01 +0530

    Time: 612 ms (7.91%), Space: 135.6 MB (18.68%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,26 +2,49 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         
+//         int n = nums.size();
+//         deque<int> dq;
+//         vector<int> sum(n);
+//         sum[0] = nums[0];
+        
+//         dq.push_back(0);
+        
+//         for(int i = 1; i < nums.size(); i++){
+            
+//             while(!dq.empty() and dq.front() < i - k)
+//                 dq.pop_front();
+            
+//             sum[i] = sum[dq.front()] + nums[i];
+            
+//             while(!dq.empty() and sum[dq.back()] <= sum[i])
+//                 dq.pop_back();
+            
+//             dq.push_back(i);
+//         }
+        
+//         return sum.back();
+        
+        // solution 2 
+        
+        
         int n = nums.size();
-        deque<int> dq;
-        vector<int> sum(n);
-        sum[0] = nums[0];
+        multiset<int> mp;
+        vector<int> dp(n);
         
-        dq.push_back(0);
+        dp[0] = nums[0];
+        mp.insert(dp[0]);
         
-        for(int i = 1; i < nums.size(); i++){
+        for(int i = 1; i < n; i++){
             
-            while(!dq.empty() and dq.front() < i - k)
-                dq.pop_front();
+            dp[i] = nums[i] + *mp.rbegin();
             
-            sum[i] = sum[dq.front()] + nums[i];
+            if(i >= k)
+                mp.erase(mp.find(dp[i-k]));
             
-            while(!dq.empty() and sum[dq.back()] <= sum[i])
-                dq.pop_back();
-            
-            dq.push_back(i);
+            mp.insert(dp[i]);
         }
         
-        return sum.back();
+        
+        return dp[n-1];
     }
 };

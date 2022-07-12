f06ff0b18a26b35730b6456f593a12cef981ffad
bhannu galhotra <42721076+Bhannugalhotra@users.noreply.github.com>
2022-07-12 14:58:52 +0530

    Time: 289 ms (49.89%), Space: 89.5 MB (49.86%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -2,24 +2,48 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         
-        vector<pair<int,int>> zip;
+//         vector<pair<int,int>> zip;
         
-        for(int i = 0; i < capacity.size() ;i++){
-            zip.push_back({rocks[i], capacity[i]});
-        }
+//         for(int i = 0; i < capacity.size() ;i++){
+//             zip.push_back({rocks[i], capacity[i]});
+//         }
         
-        sort(begin(zip), end(zip) ,[] (pair<int,int> a, pair<int,int> b){
+//         sort(begin(zip), end(zip) ,[] (pair<int,int> a, pair<int,int> b){
                                         
-                                        return (a.second - a.first) < (b.second - b.first);
-                                        });
+//                                         return (a.second - a.first) < (b.second - b.first);
+//                                         });
+        
+//         int ans = 0;
+        
+//         for(pair<int,int> &it : zip){
+            
+//             int diff = it.second - it.first;
+            
+//             additionalRocks -= diff;
+            
+//             if(additionalRocks < 0)
+//                 break;
+            
+//             ans++;
+//         }
+        
+//         return ans;
+        
+        
+        // solution 2
+        
+        vector<int> diff;
+        
+        for(int i = 0; i < capacity.size(); i++)
+            diff.push_back(capacity[i] - rocks[i]);
+        
+        sort(begin(diff), end(diff));
         
         int ans = 0;
         
-        for(pair<int,int> &it : zip){
+        for(int it : diff){
             
-            int diff = it.second - it.first;
-            
-            additionalRocks -= diff;
+            additionalRocks -= it;
             
             if(additionalRocks < 0)
                 break;

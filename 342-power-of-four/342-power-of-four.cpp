b0ac35cb43b0ced60865132cf2c0381814358dbd
bhannu galhotra <42721076+Bhannugalhotra@users.noreply.github.com>
2022-08-22 11:26:30 +0530

    Time: 4 ms (40.16%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
+    
+    
+    int idxOfSetBit(int n){
         
-        while(n > 1 and n % 4 == 0){
-            n /= 4;
+        for(int i = 31 ; i >= 0; i--){
+            if(n & (1 << i))
+                return i;
         }
         
-        return n == 1;
+        return -1;
+    }
+    bool isPowerOfFour(int n) {
+        
+//         while(n > 1 and n % 4 == 0){
+//             n /= 4;
+//         }
+        
+//         return n == 1;
+        
+        return n > 0 and (( n & (n-1) ) == 0) and idxOfSetBit(n) % 2 == 0;
     }
 };
